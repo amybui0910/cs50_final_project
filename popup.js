e3b5ae1294dcff2c7a64936a94a76d5a6031db26
Your Name Comes Here <amybui0910@gmail.com>
2024-10-07 13:16:20 -0400

    close popup via button functionality

--- popup.js
+++ popup.js
@@ -1,20 +1,32 @@
-    let name = "flashcards"
+let name = "flashcards"
 
-    if (!(name in localStorage)) {
-        document.querySelector('h1').innerHTML = "No cards created";
+let flashcards = localStorage.getItem(name); 
+
+if (!flashcards) {
+    document.querySelector('h1').innerHTML = "No cards created";
+} else {
+    let cards = JSON.parse(flashcards); 
+    if (cards.length > 0) {
+        let index = Math.floor(Math.random() * cards.length); 
+
+        document.querySelector('h1').innerHTML = `${cards[index]["front"]}`;
+        document.querySelector('body').innerHTML += "<button id='flip'>Flip</button>";
+
+        document.querySelector('#flip').addEventListener('click', function() {
+            document.querySelector('body').innerHTML += "<h2>" + cards[index]["back"] + "</h2>";
+        });
+
     } else {
-        let cards = JSON.parse(localStorage.getItem(name)); 
-        if (cards.length > 0) {
-            let index = Math.floor(Math.random() * cards.length); 
-
-            document.querySelector('h1').innerHTML = `${cards[index]["front"]}`;
-            document.querySelector('body').innerHTML += "<button id='flip'>Flip</button>";
-
-            document.querySelector('button').addEventListener('click', function() {
-                document.querySelector('body').innerHTML += "<h2>" + cards[index]["back"] + "</h2>";
-            });
-            
-        } else {
-            document.querySelector('h1').innerHTML = "No cards created";
-        }
+        document.querySelector('h1').innerHTML = "No cards created";
     }
+}
+
+let closeButton = document.querySelector('#close');
+if (closeButton) {
+    closeButton.addEventListener('click', function() {
+        // Close the current window
+        chrome.windows.getLastFocused({populate: false}, function(window) {
+            chrome.windows.remove(window.id);
+        });
+    });
+}
